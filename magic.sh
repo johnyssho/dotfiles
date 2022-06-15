6e100b5c45da747a4b5de45fa46c75aa37af3b1a
johnyssho <hon.brod@email.cz>
2022-06-15 16:34:32 +0200

    firstone

--- magic.sh
+++ magic.sh
@@ -4,11 +4,12 @@ sudo pacman -S --needed - <pkglist.txt
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git 
 cd yay
 makepkg -si
+cd ..
 yay -S - <pkglist_aur.txt
 sudo usermod --shell /bin/zsh jh
 cp .zshrc ~/
 mv config/* ~/.config/
 mkdir -p ~/.local/src
-cp -r src/* ~/.local/src/
+sudo cp -r src/* ~/.local/src/
 cd ~/.local/src
 #sudo ln -s ~/.local/bin/* /usr/local/bin/
